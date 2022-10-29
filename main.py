8cbb8a8dc1d9daaac26a9909b7e5c0e338588cc3
Rinat <61133934+Renatkg20@users.noreply.github.com>
2022-10-29 22:04:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+# User input the year and program gives result accourding Chines Calendar
+user = input("Please input the year: ")
+
+lis1 = {1:"Cock",2:"Dog",3:"Pig",4:"Rate",5:"Bull",6:"Tiger", 7:"Hare",8:"Dragon",9:"Snake",10:"Horse",11:"Sheep",0:"Monkey"}
+
+ans = int(user) % 12
+
+print(lis1.get(ans))
